1caa859448b64cbda2a32bca157622502d043889
yunus-atmaca <yunusatmacabm@gmail.com>
2023-04-04 16:40:55 +0300

    add splash screen, app icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import SplashScreen from 'react-native-splash-screen'
+import Orientation from 'react-native-orientation-locker'
+
+import { useSession } from '@src/hooks/app'
 
 const App = () => {
+  const appSession = useSession()
+
+  useEffect(() => {
+    Orientation.lockToPortrait()
+
+    if (appSession === 'LOADING') {
+    } else if (appSession === 'LOADED') {
+      SplashScreen.hide()
+    } else if (appSession === 'FAILED') {
+    }
+  }, [appSession])
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <SafeAreaView style={{}}></SafeAreaView>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <SafeAreaView style={{ flex: 1 }}></SafeAreaView>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   )
 }
