7dfab0a7c1acfa22525135d344fa7754570d690f
yunus-atmaca <yunusatmacabm@gmail.com>
2023-04-12 23:03:54 +0300

    add profile tabs

---
+++ src/screens/HomeRoutes/Profile/SelfMedia.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+import {} from 'react-native'
+
+import {} from 'react-native-size-matters'
+
+const SelfMedia:FC = () =>{
+
+  return null
+}
+
+export default SelfMedia
